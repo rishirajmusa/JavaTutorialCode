6815928e80417074aef829f3f43bfefb9c22e7bd
rishirajmusa <rishirajmusa1999@gmail.com>
2023-06-08 21:36:15 +0530

    Encapsulation Project

---
+++ All Programs/Encapsulation/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Student s= new Student();
+		s.setName("Rahul");
+		s.setAge(25);
+		System.out.println(s.getName());
+		System.out.println(s.getAge());
+	}
+
+}
+
