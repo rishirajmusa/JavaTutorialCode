d41281c94e805980e9bf92c87b32a8ecf66d1868
rishirajmusa <rishirajmusa1999@gmail.com>
2023-06-09 11:54:04 +0530

    Dice Roll 1-6 Random Output

--- All Programs/Dice.java
+++ All Programs/Dice.java
@@ -2,9 +2,9 @@ package diceRollPackage;
 
 import java.util.Random;
 
-public class Dice {
+public  class Dice {
 
-	public static void main(String[] args) {
+	public  static void main(String[] args) {
 		Random x =new Random();
 		int i = x.nextInt(6)+1;//+1 is added here because x.nextInt(6) will return value from 0-5, by adding 1 we can make it 1-6
 		System.out.println(" You Rolled a : "+i );
