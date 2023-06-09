e0385c745c403f2059afa3915482a780ad76ceee
rishirajmusa <rishirajmusa1999@gmail.com>
2023-06-09 12:02:37 +0530

    foreach loop

--- All Programs/ForEachTutotrial.java
+++ All Programs/ForEachTutotrial.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ForEachTutotrial {
+public  class ForEachTutotrial {
 
-	public static void main(String[] args) {
+	public  static void main(String[] args) {
 		//In Java, the for-each loop is used to
 		//iterate through elements of arrays and collections (like ArrayList)
 	ArrayList<Integer> myMarks = new ArrayList<Integer>(Arrays.asList(10,20,50,400,500,453));
