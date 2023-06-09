13f2a7d8cebbf9a592d9ea1296196d9a3ad2d5a2
rishirajmusa <rishirajmusa1999@gmail.com>
2023-06-09 11:52:00 +0530

    Argument And Parameters

--- All Programs/ArgumentsAndParameters.java
+++ All Programs/ArgumentsAndParameters.java
@@ -1,8 +1,8 @@
 package ArgumentsAndParametersPackage;
 
-public class ArgumentsAndParameters {
+public  class ArgumentsAndParameters {
 
-	public static void main(String[] args) {
+	public  static void main(String[] args) {
 	saySomething("Hi! Nice to meet You.");//here we write the value of string s of the method saySomething and it's called Arguments
 	add(5,9);//here i passed the arguments of method add. This is called Method Parameters.
 	//this is the most important step in programming.
@@ -16,13 +16,13 @@ public class ArgumentsAndParameters {
 	int result5=multiply(4,12);
 	System.out.println(result1 +"\n"+ result2 +"\n"+ result3 +"\n"+ result4 +"\n"+  result5 +"\n");
 	}
-	public static void saySomething(String s) {
+	public  static void saySomething(String s) {
 		System.out.println(s);//here in above line and in this line s is a parameter and it stays the same
 	}
-	public static void add(int x, int y) {
+	public  static void add(int x, int y) {
 		System.out.println(x+y);
 	}
-	public static int multiply(int x, int y) {
+	public  static int multiply(int x, int y) {
 		return x*y;//here i have specified the parameter in above line and method in this.
 		// now i just have to call this method with needed arguments
 		// i can use this as many time as needed 
