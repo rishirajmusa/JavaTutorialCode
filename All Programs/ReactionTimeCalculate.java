924748142fe40f37166aa54ca9dc47bc27685849
rishirajmusa <rishirajmusa1999@gmail.com>
2023-06-10 13:13:02 +0530

    Reaction Time Calculator - Calculate the reaction time by taking input from user and calculate the time in between

---
+++ All Programs/ReactionTimeCalculate.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReactionTimeCalculate {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("--------Reaction Time Calculator--------");
+		System.out.println("Insert the value at GO!!! and press Enter");
+		Scanner scan = new Scanner(System.in);
+		try {
+		System.out.print("Press Enter to Continue...");
+		System.in.read();
+		}catch(Exception e) {}
+		System.out.println("3");
+		Thread.sleep(800);
+		System.out.println("2");
+		Thread.sleep(800);
+		System.out.println("1");
+		Thread.sleep(800);
+		System.out.println("GO!!!");
+		long startTime=System.currentTimeMillis();
+		scan.next();
+		long stopTime=System.currentTimeMillis();
+		long reactionTime= stopTime - startTime;
+		System.out.println("You reaction time is : "+reactionTime);
+	}
+
+}
+
