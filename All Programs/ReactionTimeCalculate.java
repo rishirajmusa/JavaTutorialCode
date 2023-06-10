ca457011d652dcaa89bc32d28c5e22c616cb6a1a
Rishi Raj <135038085+rishirajmusa@users.noreply.github.com>
2023-06-10 17:14:31 +0530

    Update ReactionTimeCalculate.java
    Reaction Time Calculator
    
    closed the scanner

--- All Programs/ReactionTimeCalculate.java
+++ All Programs/ReactionTimeCalculate.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
 public class ReactionTimeCalculate {
-
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("--------Reaction Time Calculator--------");
 		System.out.println("Insert the value at GO!!! and press Enter");
-		Scanner scan = new Scanner(System.in);
 		try {
 		System.out.print("Press Enter to Continue...");
 		System.in.read();
@@ -20,6 +19,7 @@ public class ReactionTimeCalculate {
 		long startTime=System.currentTimeMillis();
 		scan.next();
 		long stopTime=System.currentTimeMillis();
+		scan.close();
 		long reactionTime= stopTime - startTime;
 		System.out.println("You reaction time is : "+reactionTime);
 	}
